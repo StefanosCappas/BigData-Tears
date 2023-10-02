120babdd65cc32867eef9752231b1676dae93041
StefanosCappas <119065927+StefanosCappas@users.noreply.github.com>
2023-10-02 23:08:37 +0300

    Οι κώδικες Spark για παραγωγή inline JSON.

---
+++ pySparkUS.py
@@ -0,0 +1,81 @@
+from pyspark.sql import SparkSession
+if __name__ == "__main__":
+	spark = SparkSession.builder.appName("Read corrupted json").getOrCreate()
+  # Διάβασμα των 2 αρχείων JSON, το ένα με το GDP και το άλλο με το POP και εμφάνιση χύμα στην οθόνη.
+	dfFromTxt1=spark.read.text("./drive/MyDrive/US.NY.GDP.MKTP.CD.json")
+	dfFromTxt2=spark.read.text("./drive/MyDrive/US.SP.POP.json")
+	print("Το 1ο αρχείο προς εξέταση πριν από την επεξεργασία του, δηλαδή το US.NY.GDP.MKTP.CD.json")
+	dfFromTxt1.show(truncate=False)
+	print("Το 2ο αρχείο προς εξέταση πριν από την επεξεργασία του, δηλαδή το US.SP.POP.json")
+	dfFromTxt2.show(truncate=False)
+  # Περιοχή επεξεργασίας και περικοπής του JSON αρχείου.
+	from pyspark.sql.functions import regexp_replace
+  # Γίνεται η επεξεργασία στο 1ο αρχείο, δηλαδή αυτό που περιέχει το gdp.
+  # Γίνεται ο εντοπισμός αλλαγής γραμμής στο αρχείο όταν τελειώσουν όλα τα πεδία του και ξεκινάει νέα γραμμή.
+	dfFromTxt1 = dfFromTxt1.withColumn('value', regexp_replace('value', '\},\{', '\}__SPLIT_POINT__\{'))
+  # Αφαιρείται η 1η γραμμή που αφορά την επικεφαλίδα του JSON επειδή δεν μας χρειάζεται και επίσης προκαλεί πρόβλημα στην ανάγνωση.
+	dfFromTxt1 = dfFromTxt1.withColumn('value', regexp_replace('value', '\[\{"page":1,"pages":1,"per_page":5000,"total":63,"sourceid":"2","lastupdated":"2023-07-25"\},\[','\['))
+	# Αφαιρούνται τα εμφωλευμένα πεδία με την χρήση διπλού ονόματος εξωτερικού και εσωτερικού πεδίου μαζί στο indicator.
+	dfFromTxt1 = dfFromTxt1.withColumn('value', regexp_replace('value', '"indicator":\{"id"','"indicatorId"'))
+	dfFromTxt1 = dfFromTxt1.withColumn('value', regexp_replace('value', '"value":"GDP \(current US\$\)"','"indicatorValue":"GDP \(current US\$\)"'))
+	# Η ίδια διαδικασία για το country.
+	dfFromTxt1 = dfFromTxt1.withColumn('value', regexp_replace('value', '\},"country":\{"id"',',"countryId"'))
+	dfFromTxt1 = dfFromTxt1.withColumn('value', regexp_replace('value', '"value":"United States"','"countryValue":"United States"'))
+  # Εδώ αφαιρούμε την παρένθεση \} που κλείνει την εμφωλευμένη περιοχή για αποφυγή σφαλμάτων.
+	dfFromTxt1 = dfFromTxt1.withColumn('value', regexp_replace('value', '\},', ','))
+  # Μετονομάζουμε το πεδίο του value σε gdp επειδή εμφανίζει το gdp.
+	dfFromTxt1 = dfFromTxt1.withColumn('value', regexp_replace('value', 'value', 'gdp'))
+  # Η ίδια διαδικασία που κάναμε στο 1ο αρχείο με την διαφορά ότι το πεδίο value αντιπροσωπεύει τον πλυθισμό (population) και το πεδίο decimal
+  # που αντιπροσωπεύει μία νέα στήλη με μηδενικά όπου εκεί θα αποθηκευτεί το gdpPerCapita για αυτό τον λόγο θα μετονομαστεί έτσι.
+	dfFromTxt2 = dfFromTxt2.withColumn('value', regexp_replace('value', '\},\{', '\}__SPLIT_POINT__\{'))
+	dfFromTxt2 = dfFromTxt2.withColumn('value', regexp_replace('value', '\[\{"page":1,"pages":1,"per_page":5000,"total":63,"sourceid":"2","lastupdated":"2023-07-25"\},\[','\['))
+	dfFromTxt2 = dfFromTxt2.withColumn('value', regexp_replace('value', '"indicator":\{"id"','"indicatorId"'))
+	dfFromTxt2 = dfFromTxt2.withColumn('value', regexp_replace('value', '"value":"GDP \(current US\$\)"','"indicatorValue":"GDP \(current US\$\)"'))
+	dfFromTxt2 = dfFromTxt2.withColumn('value', regexp_replace('value', '\},"country":\{"id"',',"countryId"'))
+	dfFromTxt2 = dfFromTxt2.withColumn('value', regexp_replace('value', '"value":"United States"','"countryValue":"United States"'))
+	dfFromTxt2 = dfFromTxt2.withColumn('value', regexp_replace('value', '\},', ','))
+	dfFromTxt2 = dfFromTxt2.withColumn('value', regexp_replace('value', 'value', 'population'))
+	dfFromTxt2 = dfFromTxt2.withColumn('value', regexp_replace('value', 'decimal', 'gdpPerCapita'))
+  # Εμφάνιση των 2 αρχείων JSON στην οθόνη μετά από την αρχική τους επεξεργασία.
+	print("\nΤο 1ο αρχείο προς εξέταση μετά από την επεξεργασία του")
+	dfFromTxt1.show(truncate=False)
+	print("Το 2ο αρχείο προς εξέταση μετά από την επεξεργασία του")
+	dfFromTxt2.show(truncate=False)
+  # Δημιουργία 2 σχημάτων για τα JSON αρχεία. 1 για το καθένα. Υπενθυμίζω ότι δεν βάζω στο σχήμα όλα τα πεδία των αρχείων αλλά μόνο αυτά
+  # που θα χρησιμοποιήσω στο μέλλον για τροποποίηση.
+	from pyspark.sql.types import StructType,StructField,IntegerType,StringType,DoubleType,BooleanType, DateType
+	schema1 = StructType([
+    StructField("countryValue", StringType(), True),
+    StructField("date", DateType(), True),
+    StructField("gdp", DoubleType(), True),
+  ])
+	schema2 = StructType([
+    #StructField("countryValue", StringType(), True),
+    StructField("date", DateType(), True),
+    StructField("population", IntegerType(), True),
+    StructField("gdpPerCapita", DoubleType(), True)
+  ])
+  # Στα 2 αρχεία κάνω εξαγωγή των γραμμών όταν συναντήσουν το πεδίο αλλαγής γραμμής ή __SPLIT_POINT__.
+	from pyspark.sql.functions import split, explode
+	dfFromTxt1 = dfFromTxt1.withColumn('value',explode(split('value','__SPLIT_POINT__')))
+	dfFromTxt2 = dfFromTxt2.withColumn('value',explode(split('value','__SPLIT_POINT__')))
+  # Εδώ βάζω τα 2 αρχεία που επεξεργάστηκα πριν στο σχήμα που αντιστοιχούν σε αυτά.
+	from pyspark.sql.functions import col,from_json
+	dfJSON1 = dfFromTxt1.withColumn("jsonData",from_json(col("value"),schema1)).select("jsonData.*").dropna()
+	dfJSON2 = dfFromTxt2.withColumn("jsonData",from_json(col("value"),schema2)).select("jsonData.*").dropna()
+  # Εμφάνιση των 2 αρχείων στην μορφή του σχήματος τους.
+	print("\nΕμφανίζεται το 1ο αρχείο μετά την τοποθέτηση του σχήματος του")
+	dfJSON1.show(truncate=False)
+	print("Εμφάνιζεται το 2ο αρχείο μετά την τοποθέτηση του σχήματος του")
+	dfJSON2.show(truncate=False)
+  # Γίνεται η συννένωση των 2 αρχείων με κλειδί το date, και εμφάνιση του αρχείου στην οθόνη.
+	dfJSON = dfJSON1.join(dfJSON2, on=["date"], how='inner')
+	print("\nΕμφανίζεται τα 2 αρχεία JSON συγχωνεμένα σε 1 για τον υπολογισμό του gdp per capita")
+	dfJSON.show()
+  # Αποθηκεύεται το αποτέλεσμα της πράξης των gdp / population στην στήλη gdpPerCapita για τον υπολογισμό του κατα κεφαλήν gdp
+  # και εμφανίζεται το συνολικό αποτέλεσμα στην οθόνη.
+	dfJSON = dfJSON.withColumn('gdpPerCapita', col('gdp') / col('population') )
+	print("\nΕμφανίζονται το ίδιο αρχείο με την αποθήκευση του gdp/population στο gdpPerCapita")
+	dfJSON.show()
+  # Αποθηκεύεται το αποτέλεσμα σε ένα νέο JSON αρχείο και τερματίζεται το πρόγμαμα.
+	dfJSON.write.format('json').mode('overwrite').save('./corrupted-json-example')
